36b38ee5c249a4130a6d861e2f9ddd44b07906bf
Seth Blanchard <nsethblanchard@gmail.com>
2021-04-01 11:33:20 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString){
+  let time = timeString.split(':');
+  let hour = parseInt(time[0]);
+  
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour > 12 && hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  
+  document.getElementById('greeting').innerText = string;
+  
+}
+
